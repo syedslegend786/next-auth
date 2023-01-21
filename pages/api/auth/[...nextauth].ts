f41bf18fa54ac6288fb26f899eb287ab782ee3eb
connectmp <syedslegend786@gmail.com>
2023-01-22 00:45:10 +0500

    added next-auth secret

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,7 +2,8 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "../../../lib/mongodb";
-export const authOptions = {
+import {AuthOptions} from 'next-auth'
+export const authOptions:AuthOptions = {
   adapter: MongoDBAdapter(clientPromise),
   // Configure one or more authentication providers
   providers: [
@@ -12,6 +13,7 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+  secret:"123123123l1k2jkhasjdhagsd"
 };
 
 export default NextAuth(authOptions);
